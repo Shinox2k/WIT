7a714200ebbc5c996b06f89320d655dad94a2ad6
Shinox2k <shinox2k@gmail.com>
2025-02-14 18:36:29 +0100

    4

--- streamlit_app.py
+++ streamlit_app.py
@@ -121,35 +121,6 @@ if "quiz_started" in st.session_state and st.session_state.quiz_started:
             st.rerun()
 
 if "show_results" in st.session_state and st.session_state.show_results:
-    st.write("Podsumowanie pytań i odpowiedzi:")  # Sekcja wyników
-
-    for idx, question_data in enumerate(st.session_state.quiz_data):
-        # Wyświetl pytanie
-        st.subheader(f"Pytanie {idx + 1}: {question_data['question']}")
-
-        # Pobierz odpowiedzi użytkownika i poprawne
-        user_selection = set(st.session_state.user_answers[question_data['question']])
-        correct_answers = set(question_data['answer'])
-
-        if user_selection != correct_answers:  # Jeśli odpowiedź użytkownika jest błędna
-            st.write("Twoje odpowiedzi:")
-            for option in question_data['options']:
-                if option in user_selection and option not in correct_answers:
-                    # Błędnie zaznaczone przez użytkownika
-                    st.write(f"❌ {option} (Twoja odpowiedź)")
-                elif option in user_selection and option in correct_answers:
-                    # Zaznaczone poprawne (nie pojawi się, bo chcemy je ukryć)
-                    continue
-
-            st.write("Poprawne odpowiedzi:")
-            for option in correct_answers:
-                if option not in user_selection:
-                    st.write(f"✔ {option}")
-        else:
-            # Jeśli odpowiedź jest poprawna, wyświetl komunikat bez szczegółowych opcji
-            st.write("🎉 To pytanie zostało zaliczone poprawnie!")
-
-    # Wywołanie podsumowania ogólnego
     quiz_results(st.session_state.quiz_data, st.session_state.user_answers)
 
 st.markdown("""
